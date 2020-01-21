5b9f54dffdd45bfa32fbe68758753c08cc4fc433
15463 <1546352238@qq.com>
2020-01-21 21:37:58 +0800

    update SingeLinkedList

--- src/DataSturctures/linkedlist/SingleLinkedListDemo.java
+++ src/DataSturctures/linkedlist/SingleLinkedListDemo.java
@@ -9,16 +9,36 @@ public class SingleLinkedListDemo {
         HeroNode hero4 = new HeroNode(4, "林冲", "豹子头");
         SingleLinkedList singleLinkedList = new SingleLinkedList();
         //直接加入
-        singleLinkedList.add(hero2);
-        singleLinkedList.add(hero3);
-        singleLinkedList.add(hero1);
-        singleLinkedList.add(hero4);
+//        singleLinkedList.add(hero2);
+//        singleLinkedList.add(hero3);
+//        singleLinkedList.add(hero1);
+//        singleLinkedList.add(hero4);
+
         //加入按编号
         singleLinkedList.addByOrder(hero2);
         singleLinkedList.addByOrder(hero3);
         singleLinkedList.addByOrder(hero1);
         singleLinkedList.addByOrder(hero4);
         singleLinkedList.list();
+
+//        System.out.println("修改后的节点");
+
+        //修改链表节点
+//        HeroNode newHero3 = new HeroNode(3, "吴用~", "智多星");
+//        singleLinkedList.update(newHero3);
+
+        //删除链表节点
+//        int no = 4;
+//        singleLinkedList.delete(no);
+
+//        singleLinkedList.list();
+
+        //打印链表中有效节点的个数
+//        System.out.printf("链表的节点个数：%d", SingleLinkedList.getLength(singleLinkedList.getHead()));
+        //测试打印单链表的倒数第k个节点
+
+        int index = 1;
+        System.out.printf("链表中倒数第 %d 个节点为: %s",index,singleLinkedList.getLastIndexNode(singleLinkedList.getHead(),index));
     }
 }
 
@@ -26,6 +46,10 @@ public class SingleLinkedListDemo {
 class SingleLinkedList {
     //先初始化一个头节点，头节点不允许更改，不存放具体数据
     private HeroNode head = new HeroNode(0, "", "");
+
+    public HeroNode getHead() {
+        return head;
+    }
 
     //添加节点到单链表，不考虑编号顺序
     //1.找到当前链表的最后节点
@@ -68,6 +92,98 @@ class SingleLinkedList {
             heroNode.next = temp.next;
             temp.next = heroNode;
         }
+    }
+
+    //修改链表节点，根据no
+    public void update(HeroNode newHeroNode) {
+        if (head.next == null) {
+            System.out.println("链表为空，无法修改！");
+            return;
+        }
+        HeroNode temp = head.next;
+        boolean flag = false; //是否找到该节点
+        while (true) {
+            //已到达链表最后
+            if (temp == null) {
+                break;
+            }
+            if (temp.no == newHeroNode.no) {
+                //表示已经找到
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            temp.name = newHeroNode.name;
+            temp.nickName = newHeroNode.nickName;
+        } else {
+            System.out.println("此链表中没有此no");
+        }
+    }
+
+    //删除节点
+    //temp.next.no 和 需要删除的节点的no 进行比较
+    //删除节点 temp.next = temp.next.next，无引用的会被GC
+    public void delete(int no) {
+        HeroNode temp = head;
+        boolean flag = false;
+        while (true) {
+            //已经到链表的最后
+            if (temp.next == null) {
+                break;
+            }
+            //找到待删除节点的前一个节点temp
+            if (temp.next.no == no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;  //temp后移，遍历
+        }
+        //如果找到该节点
+        if (flag) {
+            temp.next = temp.next.next;
+        } else {
+            System.out.println("此链表中未找到要删除的节点！");
+        }
+    }
+
+    //获取链表的节点个数（如果是带头节点的链表，不统计头节点）
+    public static int getLength(HeroNode head) {
+        //空链表
+        if (head.next == null) {
+            return 0;
+        }
+        int length = 0;
+        HeroNode cur = head.next;
+        while (cur != null) {
+            length++;
+            cur = cur.next;
+        }
+        return length;
+    }
+
+    //查找单链表中倒数第k个节点
+    /**
+     * @param head  链表头节点
+     * @param index 表示倒数第index个节点
+     */
+    public HeroNode getLastIndexNode(HeroNode head, int index) {
+        //该链表为空
+        if (head.next == null) {
+            return null;
+        }
+        int size = getLength(head);
+        //对index的校验
+        if (index <= 0 || index > size) {
+            return null;
+        }
+        //辅助变量
+        HeroNode cur = head.next;
+        for (int i = 0;i<size-index;i++){
+            cur = cur.next;
+        }
+        return cur;
     }
 
     //显示链表

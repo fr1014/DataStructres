fe142cb0a3ef7db87cbd76c20f7acaa94ea69186
范瑞 <5565778+fan_rui1014@user.noreply.gitee.com>
2020-03-24 18:03:09 +0800

    增加二叉树的前、中、后序遍历查找

--- src/DataSturctures/tree/BinaryTreeDemo.java
+++ src/DataSturctures/tree/BinaryTreeDemo.java
@@ -9,19 +9,28 @@ public class BinaryTreeDemo {
         HeroNode node2 = new HeroNode(2, "吴用");
         HeroNode node3 = new HeroNode(3, "卢俊义");
         HeroNode node4 = new HeroNode(4, "林冲");
+        HeroNode node5 = new HeroNode(5, "关胜");
 
         root.setLeft(node2);
         root.setRight(node3);
         node3.setRight(node4);
+        node3.setLeft(node5);
 
         binaryTree.setRoot(root);
 
-        System.out.println("前序遍历");
-        binaryTree.preOrder();  //1,2,3,4
-        System.out.println("中序遍历");
-        binaryTree.infixOrder(); //2,1,3,4
+        System.out.println("前序遍历：");
+        binaryTree.preOrder();  //1,2,3,5,4
+        System.out.println("前序遍历查找：");
+        System.out.println(binaryTree.preOrderFind(5));
+
+        System.out.println("中序遍历：");
+        binaryTree.infixOrder(); //2,1,5,3,4
+        System.out.println("中序遍历查找：");
+        System.out.println(binaryTree.infixOrderFind(5));
         System.out.println("后序遍历");
-        binaryTree.postOrder(); //2,4,3,1
+        binaryTree.postOrder(); //2,5,4,3,1
+        System.out.println("后序遍历查找：");
+        System.out.println(binaryTree.postOrderFind(5));
     }
 }
 
@@ -65,6 +74,45 @@ class BinaryTree {
         } else {
             System.out.println("此二叉树为空，无法遍历！");
         }
+    }
+
+    /**
+     * 前序遍历查找
+     *
+     * @param id 需要查找的id
+     * @return
+     */
+    public HeroNode preOrderFind(int id) {
+        if (this.root != null) {
+            return this.root.preOrderFind(id);
+        }
+        return null;
+    }
+
+    /**
+     * 中序遍历查找
+     *
+     * @param id 需要查找的id
+     * @return
+     */
+    public HeroNode infixOrderFind(int id){
+        if (this.root!=null){
+            return this.root.infixOrderFind(id);
+        }
+        return null;
+    }
+
+    /**
+     * 后序遍历查找
+     *
+     * @param id 需要查找的id
+     * @return
+     */
+    public HeroNode postOrderFind(int id){
+        if (this.root!=null){
+            return this.root.postOrderFind(id);
+        }
+        return null;
     }
 }
 
@@ -150,6 +198,81 @@ class HeroNode {
         }
         //输出父节点
         System.out.println(this);
+    }
+
+    /**
+     * 前序遍历查找
+     *
+     * @param id 需要查找的id
+     * @return
+     */
+    public HeroNode preOrderFind(int id) {
+        //判断当前节点是否为要查找的id
+        if (this.id == id) {
+            return this;
+        }
+        //1.判断当前节点的左子节点是否为空，如果非空，则递归前序遍历
+        //2.如果左递归前序查找，找到节点，则返回
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.preOrderFind(id);
+        }
+
+        if (resNode != null) {
+            return resNode;
+        }
+
+        if (this.right != null) {
+            resNode = this.right.preOrderFind(id);
+        }
+        return resNode;
+    }
+
+    /**
+     * 中序遍历查找
+     *
+     * @param id
+     * @return
+     */
+    public HeroNode infixOrderFind(int id) {
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.infixOrderFind(id);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+        if (this.id == id) {
+            return this;
+        }
+        if (this.right != null) {
+            resNode = this.right.infixOrderFind(id);
+        }
+        return resNode;
+    }
+
+    /**
+     * 后序遍历查找
+     * @param id
+     * @return
+     */
+    public HeroNode postOrderFind(int id) {
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.infixOrderFind(id);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+
+        if (this.right != null) {
+            resNode = this.right.infixOrderFind(id);
+        }
+
+        if (this.id == id) {
+            return this;
+        }
+        return resNode;
     }
 
     @Override

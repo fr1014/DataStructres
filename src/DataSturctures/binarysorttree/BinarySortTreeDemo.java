fc9f0e7edd84bc6a04baf0a530e89f647a16443c
范瑞 <5565778+fan_rui1014@user.noreply.gitee.com>
2020-08-04 23:17:33 +0800

    二叉排序树

---
+++ src/DataSturctures/binarysorttree/BinarySortTreeDemo.java
@@ -0,0 +1,115 @@
+package DataSturctures.binarysorttree;
+
+public class BinarySortTreeDemo {
+    public static void main(String[] args) {
+        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        BinarySortTree binarySortTree = new BinarySortTree();
+        //循环的添加结点到二叉排序树
+        for (int value : arr) {
+            binarySortTree.add(new Node(value));
+        }
+
+        binarySortTree.infixOrder();
+    }
+
+
+}
+
+class BinarySortTree {
+    private Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    //添加节点的方法
+    public void add(Node node) {
+        if (root == null) {
+            root = node; //如果root为空则直接让root指向node
+        } else {
+            root.add(node);
+        }
+    }
+
+    //中序遍历
+    public void infixOrder() {
+        if (root != null) {
+            root.infixOrder();
+        } else {
+            System.out.println("此树为空！！！");
+        }
+    }
+}
+
+class Node {
+    private int value;
+    private Node left;
+    private Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                '}';
+    }
+
+    //添加结点的方法
+    //递归形式添加结点，注意需要满足二叉排序树的要求
+    public void add(Node node) {
+        if (node == null) {
+            return;
+        }
+        //判断传入的结点的值，和当前子树的根结点的值关系
+        if (node.value < this.value) {
+            //如果当前结点左子结点为null
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                //递归的向左子树添加
+                this.left.add(node);
+            }
+        } else {//添加的结点的值大于当前结点的值
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                //递归的向右子树添加
+                this.right.add(node);
+            }
+        }
+    }
+
+    //中序遍历
+    public void infixOrder() {
+        if (this.left != null) {
+            this.left.infixOrder();
+        }
+        System.out.println(this);
+        if (this.right != null) {
+            this.right.infixOrder();
+        }
+    }
+}
